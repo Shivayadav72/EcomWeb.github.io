8358193ab04a64fb21aad0b11b5c441f2943598a
Shiva Yadav <sy0871125@gmail.com>
2025-05-29 23:03:42 +0530

    addedagain1

--- ecom/settings.py
+++ ecom/settings.py
@@ -78,12 +78,12 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': os.environ['DB_PASSWORD_YO'],
-        'HOST': 'postgres.railway.internal',
-        'PORT': '5432', 
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'railway',
+    'USER': 'postgres',
+    'PASSWORD': os.environ['DB_PASSWORD_YO'],
+    'HOST': 'metro.proxy.rlwy.net',
+    'PORT': '31155', 
         
     }
 }
