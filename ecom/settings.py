76b776496d6a359d82371fe03ace5f895c556a82
Shiva Yadav <sy0871125@gmail.com>
2025-05-29 21:37:06 +0530

    addedagain

--- ecom/settings.py
+++ ecom/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': os.environ['DB_PASSWORD_YO'],
-        'HOST': 'metro.proxy.rlwy.net',
-        'PORT': '', 
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432', 
         
     }
 }
